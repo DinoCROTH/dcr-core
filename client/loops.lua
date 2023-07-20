e1d73b8265b716706b47dcfe81be97de47f08445
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2023-07-20 08:31:14 +0200

    Update loops.lua

--- client/loops.lua
+++ client/loops.lua
@@ -7,17 +7,17 @@ CreateThread(function()
     while true do
         Wait(0)
         if isLoggedIn then
-            Wait((1000 * 60) * QBConfig.UpdateInterval)
-            TriggerServerEvent('QBCore:UpdatePlayer')
+            Wait((1000 * 60) * DCConfig.UpdateInterval)
+            TriggerServerEvent('DCCore:UpdatePlayer')
         end
     end
 end)
 
 CreateThread(function()
     while true do
-        Wait(QBConfig.StatusInterval)
+        Wait(DCConfig.StatusInterval)
         if isLoggedIn then
-            if QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0 then
+            if DCCore.PlayerData.metadata['hunger'] <= 0 or DCCore.PlayerData.metadata['thirst'] <= 0 then
                 local ped = PlayerPedId()
                 local currentHealth = GetEntityHealth(ped)
                 SetEntityHealth(ped, currentHealth - math.random(5, 10))
